4170f0812e3fee4492f71857f131e0f73b6b3c16
Krystal Folkes <krystl.folkes@gmail.com>
2015-10-17 23:14:45 +0100

    Up to date with twillo integrated.
    twill messaging integrated finally it was so hard and annoying.

--- vidimail/vidimail.js
+++ vidimail/vidimail.js
@@ -1,8 +1,10 @@
 Tasks = new Mongo.Collection("tasks");
- 
+
+
 if (Meteor.isClient) {
+
   // This code only runs on the client
-    Template.body.helpers({
+  Template.body.helpers({
     tasks: function () {
       // Show newest tasks at the top
       return Tasks.find({}, {sort: {createdAt: -1}});
@@ -25,8 +27,11 @@ if (Meteor.isClient) {
  
       // Clear form
       event.target.text.value = "";
+      
+
     }
   });
+
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
@@ -40,3 +45,29 @@ if (Meteor.isClient) {
   });
 
 }
+
+
+if(Meteor.isServer) {
+// Configure the Twilio client
+  var ACCOUNT_SID = "AC05aea60b815f99cad88996a8b63d87ae";// SID tied to your Twilio account
+  var AUTH_TOKEN = "5efadb1e45c24fc3e13302c0bfa60d4e";
+  twilio = Twilio(ACCOUNT_SID, AUTH_TOKEN);
+  twilio.sendSms({
+    to:'+447402084758', // Any number Twilio can deliver to
+    from: '+12057373887', // A number you bought from Twilio and can use for outbound communication
+    body: 'krystal mother.' // body of the SMS message
+  }, function(err, responseData) { //this function is executed when a response is received from Twilio
+    if (!err) { // "err" is an error received during the request, if any
+      // "responseData" is a JavaScript object containing data received from Twilio.
+      // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+      // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+      console.log(responseData.from); // outputs "+14506667788"
+      console.log(responseData.body); // outputs "word to your mother."
+    } else {
+      console.log("there was a twilio error: " + JSON.stringify(err, null, 2) );
+    }
+});
+
+}
+
+
